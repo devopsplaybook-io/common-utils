6459ab14700ca7bd26caaa7f5c13115e4a1997a0
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2026-08-26 08:45:39 +0800

    feat: add Notifications client module

--- index.ts
+++ index.ts
@@ -4,5 +4,6 @@ export * from "./src/DbUtils";
 export * from "./src/DbUtilsNoTelemetry";
 export * from "./src/SqlDbUtils";
 export * from "./src/PostgresDbUtils";
+export * from "./src/Notifications";
 export * from "./src/SystemCommand";
 export * from "./src/Timeout";
